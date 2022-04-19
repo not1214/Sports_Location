157e2d0496269f986f8966dbfc4bbc67add32281
not1214 <91748221+not1214@users.noreply.github.com>
2022-04-19 23:19:06 +0900

    [Update]database(usersテーブル)

--- docker/app/Sports_Location/database/migrations/2014_10_12_000000_create_users_table.php
+++ docker/app/Sports_Location/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 30);
+            $table->string('kana', 30);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('tel')->nullable();
+            $table->date('birthday');
+            $table->enum('gender',['男性','女性','その他']);
+            $table->string('username', 40)->unique();
+            $table->string('profile_image')->nullable();
+            $table->text('introduction')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
