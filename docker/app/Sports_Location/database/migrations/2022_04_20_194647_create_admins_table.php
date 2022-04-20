d7cc55691796568134accc8b14ed2ef2a64436de
not1214 <91748221+not1214@users.noreply.github.com>
2022-04-20 23:32:21 +0900

    [Update]テーブル作成

--- docker/app/Sports_Location/database/migrations/2022_04_20_194647_create_admins_table.php
+++ docker/app/Sports_Location/database/migrations/2022_04_20_194647_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,23 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 30);
-            $table->string('kana', 30);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('tel')->nullable();
-            $table->date('birthday');
-            $table->enum('gender', ['male', 'female', 'other']);
-            $table->string('username', 40);
-            $table->string('profile_image')->nullable();
-            $table->text('introduction')->nullable();
-            $table->softDeletes();
             $table->timestamps();
-
         });
     }
 
@@ -40,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 }
---
+++ docker/app/Sports_Location/database/migrations/2022_04_20_195028_create_events_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('area_id');
+            $table->foreign('area_id')->references('id')->on('areas');
+            $table->string('event_image')->nullable();
+            $table->string('location');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->text('contents');
+            $table->text('condition');
+            $table->datetime('deadline');
+            $table->tinyInteger('number');
+            $table->text('stuff');
+            $table->text('attention');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
