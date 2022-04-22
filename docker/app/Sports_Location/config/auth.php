e5dacee2abffdca9036e6786b42f59b613fa25af
not1214 <91748221+not1214@users.noreply.github.com>
2022-04-22 19:59:32 +0900

    [Update]管理者機能

--- docker/app/Sports_Location/config/auth.php
+++ docker/app/Sports_Location/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'user',
         'passwords' => 'users',
     ],
 
@@ -40,6 +40,14 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
     ],
 
     /*
@@ -63,6 +71,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
@@ -92,6 +104,11 @@ return [
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
+        ],
+        'admins' => [
+            'provider' => 'admins',
+            'table' => 'password_resets',
+            'expire' => 60,
         ],
     ],
 
--- docker/app/Sports_Location/routes/web.php
+++ docker/app/Sports_Location/routes/web.php
@@ -19,4 +19,27 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// User認証不要
+Route::get('/', function () {
+    return redirect('/home');
+});
+
+// Userログイン後
+Route::group(['middleware' => 'auth:user'], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+//Admin認証不要
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', function () {
+        return redirect('/admin/home');
+    });
+    Route::get('login', [App\Http\Controllers\Admin\LoginController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('login', [App\Http\Controllers\Admin\LoginController::class, 'login']);
+});
+
+// Adminログイン後
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
+    Route::post('logout', [App\Http\Controllers\Admin\LoginController::class, 'logout'])->name('admin.logout');
+    Route::get('home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
+});
