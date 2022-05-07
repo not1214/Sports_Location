793747a4927575fd7e66351965f20e45a5aca3a1
not1214 <91748221+not1214@users.noreply.github.com>
2022-05-08 02:20:09 +0900

    [Update]User(コントローラ(途中))

--- docker/app/Sports_Location/routes/web.php
+++ docker/app/Sports_Location/routes/web.php
@@ -28,6 +28,24 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('events', 'App\Http\Controllers\EventController', ['except' => ['index']]);
     Route::post('events/create/confirm', [\App\Http\Controllers\EventController::class, 'createConfirm'])->name('events.create_confirm');
     Route::post('events/{event}/edit/confirm', [\App\Http\Controllers\EventController::class, 'editConfirm'])->name('events.edit_confirm');
+
+    Route::get('myPage', 'App\Http\Controllers\UserController@myPage')->name('user.myPage');
+    Route::get('myPage/edit', 'App\Http\Controllers\UserController@edit')->name('user.edit');
+    Route::post('myPage', 'App\Http\Controllers\UserController@update')->name('user.update');
+    Route::get('myPage/unsubscribe', 'App\Http\Controllers\UserController@unsubscribe')->name('user.unsubscribe');
+    Route::delete('myPage/withdraw', 'App\Http\Controllers\UserController@withdraw')->name('user.withdraw');
+    Route::post('myPage/follow', 'App\Http\Controllers\UserController@follow')->name('user.follow');
+    Route::delete('myPage/unFollow', 'App\Http\Controllers\UserController@unFollow')->name('user.unFollow');
+    Route::get('myPage/events', 'App\Http\Controllers\UserController@createdEvents')->name('user.createdEvents');
+    Route::get('myPage/pastEvents', 'App\Http\Controllers\UserController@pastEvents')->name('user.pastEvents');
+    Route::get('myPage/reservations', 'App\Http\Controllers\UserController@reservedEvents')->name('user.reservedEvents');
+    Route::get('myPage/favorites', 'App\Http\Controllers\UserController@favoriteEvents')->name('user.favoriteEvents');
+    Route::get('myPage/followings', 'App\Http\Controllers\UserController@myFollowings')->name('user.my_followings');
+    Route::get('myPage/followers', 'App\Http\Controllers\UserController@myFollowers')->name('user.my_followers');
+    Route::get('{username}', 'App\Http\Controllers\UserController@show')->name('user.show');
+    Route::get('{username}/events', 'App\Http\Controllers\UserController@events')->name('user.events');
+    Route::get('{username}/followings', 'App\Http\Controllers\UserController@followings')->name('user.followings');
+    Route::get('{username}/followers', 'App\Http\Controllers\UserController@followers')->name('user.followers');
 });
 
 //Admin認証不要
@@ -43,22 +61,4 @@ Route::group(['prefix' => 'admin'], function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
     Route::post('logout', [App\Http\Controllers\Admin\LoginController::class, 'logout'])->name('admin.logout');
     Route::get('home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
-});
-
-
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('mypage', 'App\Http\Controllers\UserController@mypage')->name('user.mypage');
-    Route::get('mypage/edit', 'App\Http\Controllers\UserController@edit')->name('user.edit');
-    Route::post('mypage', 'App\Http\Controllers\UserController@update')->name('user.update');
-    Route::get('mypage/unsubscribe', 'App\Http\Controllers\UserController@unsubscribe')->name('user.unsubscribe');
-    Route::destroy('mypage/withdraw', 'App\Http\Controllers\UserController@withdraw')->name('user.withdraw');
-    Route::get('mypage/events', 'App\Http\Controllers\UserController@created_events')->name('user.created_events');
-    Route::get('mypage/reservations', 'App\Http\Controllers\UserController@reserved_events')->name('user.reserved_events');
-    Route::get('mypage/favorites', 'App\Http\Controllers\UserController@favorited_events')->name('user.favorited_events');
-    Route::get('mypage/followings', 'App\Http\Controllers\UsersController@my_followings')->name('user.my_followings');
-    Route::get('mypage/followers', 'App\Http\Controllers\UsersController@my_followers')->name('user.my_followers');
-    Route::get('{username}', 'App\Http\Controllers\UserController@show')->name('user.show');
-    Route::get('{username}/events', 'App\Http\Controllers\UserController@events')->name('user.events');
-    Route::get('{username}/followings', 'App\Http\Controllers\UserController@followings')->name('user.followings');
-    Route::get('{username}/followers', 'App\Http\Controllers\UserController@followers')->name('user.followers');
 });
