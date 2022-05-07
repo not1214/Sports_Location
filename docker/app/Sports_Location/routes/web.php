b9cf7f0a2fd4003a3b2dc253cf05103e11d8b25e
not1214 <91748221+not1214@users.noreply.github.com>
2022-05-07 20:20:44 +0900

    [Update]User(ルーティング)

--- docker/app/Sports_Location/routes/web.php
+++ docker/app/Sports_Location/routes/web.php
@@ -44,3 +44,21 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
     Route::post('logout', [App\Http\Controllers\Admin\LoginController::class, 'logout'])->name('admin.logout');
     Route::get('home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
 });
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('mypage', 'App\Http\Controllers\UserController@mypage')->name('user.mypage');
+    Route::get('mypage/edit', 'App\Http\Controllers\UserController@edit')->name('user.edit');
+    Route::post('mypage', 'App\Http\Controllers\UserController@update')->name('user.update');
+    Route::get('mypage/unsubscribe', 'App\Http\Controllers\UserController@unsubscribe')->name('user.unsubscribe');
+    Route::destroy('mypage/withdraw', 'App\Http\Controllers\UserController@withdraw')->name('user.withdraw');
+    Route::get('mypage/events', 'App\Http\Controllers\UserController@created_events')->name('user.created_events');
+    Route::get('mypage/reservations', 'App\Http\Controllers\UserController@reserved_events')->name('user.reserved_events');
+    Route::get('mypage/favorites', 'App\Http\Controllers\UserController@favorited_events')->name('user.favorited_events');
+    Route::get('mypage/followings', 'App\Http\Controllers\UsersController@my_followings')->name('user.my_followings');
+    Route::get('mypage/followers', 'App\Http\Controllers\UsersController@my_followers')->name('user.my_followers');
+    Route::get('{username}', 'App\Http\Controllers\UserController@show')->name('user.show');
+    Route::get('{username}/events', 'App\Http\Controllers\UserController@events')->name('user.events');
+    Route::get('{username}/followings', 'App\Http\Controllers\UserController@followings')->name('user.followings');
+    Route::get('{username}/followers', 'App\Http\Controllers\UserController@followers')->name('user.followers');
+});
